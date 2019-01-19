b06d65d480e3cd569124ec57808a400d25d96c99
Ben <bdjohnson529@gmail.com>
2019-01-19 00:32:31 -0800

    add more features to GUI, including filepaths set by user

--- DKFantasyLineup.py
+++ DKFantasyLineup.py
@@ -66,7 +66,9 @@ class App():
 		self.canvasTeam19		= None
 		self.canvasTeam20		= None
 
-		self.numRecentEntry		= None
+		self.recentMatchPanel	= None
+		self.salaryPathPanel	= None
+		self.saveEntryPanel		= None
 
 		self.formatted_img_list = []
 		self.abbv_list 			= None
@@ -78,23 +80,39 @@ class App():
 
 		self.num_recent_games 	= 10
 
-	def update_historical_model(self):
-		entryString = self.numRecentEntry.get()
+	def update_settings(self):
+		recentMatchesString = self.recentMatchPanel.get()
+		salaryPathString 	= self.salaryPathPanel.get()
+		saveString 			= self.saveEntryPanel.get()
 
 		try:
-			i = int(entryString)
+			i = int(recentMatchesString)
 			if(i>0 and i<200):
 				self.num_recent_games = i
 				print "Using ", i, " recent games in the historical model."
 			else:
 				print "Number out of range. Please enter an integer between 1 and 200."
-
 		except ValueError:
 			#Handle the exception
 			print 'Please enter an integer.'
 
+		if not self.check_files(salaryPathString):
+			print "No salary file found. Please download salary CSV from DraftKings"
+		else:
+			self.salary_path = salaryPathString
+
+	def check_files(self, salaryFilePath):
+		salary_file = Path(salaryFilePath)
+		if not salary_file.is_file():
+			return False
+		else:
+			return True
+
 
 	def create_lineup_from_season(self):
+		if not self.check_files(self.salary_file):
+			print "No salary file found. Please download salary CSV from DraftKings"
+
 		print "--------------------------"
 		print "Creating lineup based on season stats"
 
@@ -186,24 +204,45 @@ class App():
 		imagesFrame = tkinter.Frame(self.window)
 		imagesFrame.pack(side="left")
 
+		spacingFrame = tkinter.Frame(self.window)
+		spacingFrame.pack(side="left")
+		tkinter.Canvas(spacingFrame, width=40, height=100).grid(row=4, column=0, sticky="w")
+
 		actionsFrame = tkinter.Frame(self.window)
 		actionsFrame.pack(side="left")
 
-
 		# Now fill up the action frame
+		tkinter.Label(actionsFrame, text="Save directory:").grid(row=0, column=0, sticky="w")
+		saveEntryPanel = tkinter.Entry(actionsFrame, width=50)
+		saveEntryPanel.insert(0, save_dir)
+		saveEntryPanel.grid(row=1, column=0, sticky="w")
+		actionsFrame.rowconfigure(1, minsize=50)
+		setattr(self, 'saveEntryPanel', saveEntryPanel)
+
+		tkinter.Label(actionsFrame, text="Location of salary file:").grid(row=2, column=0, sticky="w")
+		salaryPathPanel = tkinter.Entry(actionsFrame, width=50)
+		salaryPathPanel.insert(0, salary_path)
+		salaryPathPanel.grid(row=3, column=0, sticky="w")
+		actionsFrame.rowconfigure(3, minsize=50)
+		setattr(self, 'salaryPathPanel', salaryPathPanel)
+
+		#tkinter.Canvas(actionsFrame, width=20, height=100).grid(row=4, column=0, sticky="w")
+		tkinter.Label(actionsFrame, text="Number of recent matches to use:").grid(row=4, column=0, sticky="w")
+		recentMatchPanel = tkinter.Entry(actionsFrame)
+		recentMatchPanel.insert(0, NUM_RECENT_GAMES)
+		recentMatchPanel.grid(row=5, column=0, sticky="w")
+		actionsFrame.rowconfigure(5, minsize=50)
+		setattr(self, 'recentMatchPanel', recentMatchPanel)
+
+		tkinter.Button(actionsFrame, text="Update settings", width = 25, height=2, font=helv36,
+			command= self.update_settings).grid(row=6, column=0, sticky="w")
+
+		tkinter.Canvas(actionsFrame, width=40, height=100).grid(row=7, column=0, sticky="w")
+
 		tkinter.Button(actionsFrame, text="Lineup from Season", width = 50, height=4, font=helv36,
-			command= self.create_lineup_from_season).grid(row=0, column=0, sticky="n")
+			command= self.create_lineup_from_season).grid(row=8, column=0, sticky="w")
 		tkinter.Button(actionsFrame, text="Lineup from Recent Data", width = 50, height=4, font=helv36,
-			command= self.create_lineup_from_history).grid(row=1, column=0, sticky="n")
-
-		tkinter.Canvas(actionsFrame, width=20, height=100).grid(row=2, column=0, sticky="n")
-		tkinter.Label(actionsFrame, text="Number of recent matches to use:").grid(row=3, column=0, sticky="n")
-		entryWindow = tkinter.Entry(actionsFrame)
-		entryWindow.grid(row=4, column=0, sticky="n")
-		setattr(self, 'numRecentEntry', entryWindow)
-
-		tkinter.Button(actionsFrame, text="Update historical model", width = 25, height=2, font=helv36,
-			command= self.update_historical_model).grid(row=5, column=0, sticky="n")
+			command= self.create_lineup_from_history).grid(row=9, column=0, sticky="w")
 
 
 		# Set up the imagesFrame with canvases for our RGB and masked images
@@ -272,7 +311,6 @@ if __name__ == "__main__":
 	salary_file = Path(salary_path)
 	if not salary_file.is_file():
 		print "No salary file found. Please download salary CSV from DraftKings"
-		exit()
 
 	'''
 	Begin program
